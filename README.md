e7ff55b9e557c074104b4880e76983ca4bd99477
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-10-18 14:08:20 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chat en vivo
+
+Este es un repositorio con el ejemplo del curso de Node.js y Websockets de midudev:
+
+[https://www.youtube.com/watch?v=WpbBhTx5R9Q](https://www.youtube.com/watch?v=WpbBhTx5R9Q)
+
+ 
---
+++ server/index.js
@@ -0,0 +1,77 @@
+import express from 'express';
+import logger from 'morgan';
+
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+
+// Base de datos: Sequelize para independizar del servidor
+import { Sequelize, DataTypes } from 'sequelize';
+
+// En este caso usamos sqlite3 en memoria (se perderán los cambios al salir)
+const sequelize = new Sequelize('sqlite::memory:');
+
+// Creamos el esquema
+const Message = sequelize.define('Message', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    message: DataTypes.STRING
+});
+
+// Sincronizamos el esquema con la base de datos: si las tablas
+// no existen, se crearán
+await sequelize.sync();
+
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+
+// Creamos un servidor http de node para pasarlo a io.Server
+const server = createServer(app);
+const io = new Server(server, {
+    connectionStateRecovery: {
+
+    }
+});
+
+// Con esto ya podemos escuchar conexiones
+io.on('connection', (socket) => {
+
+    socket.on('disconnect', () => {
+        console.log("Client disconnected");
+    });
+
+    console.log("User connected");
+
+    socket.on('chat message', async msg => {
+        let result;
+        try {
+            result = await Message.create({
+                message: msg
+            });
+        }
+        catch (err) {
+            console.error(err);
+            return;
+        }
+
+
+        io.emit('chat message', msg, result.id);
+    });
+});
+
+app.use(logger('dev'));
+
+app.get('/', (req,res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+});
+
+// Escuchamos en el server, en lugar de en app
+// app.listen(port, ...
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server running at port ${port}`);
+});
